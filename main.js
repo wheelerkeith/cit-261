ceab88e06f40bae58b447488bfb0891055de113b
wheelerkeith <nemimimonkey@gmail.com>
2018-10-15 19:21:29 -0500

    Added js file

---
+++ main.js
@@ -0,0 +1,16 @@
+//button clicks
+document.getElementById('button1').addEventListener('click', sendForm);
+document.getElementById('button2').addEventListener('click', sendForm);
+document.getElementById('button3').addEventListener('click', sendForm);
+document.getElementById('button4').addEventListener('click', sendForm);
+document.getElementById('button5').addEventListener('click', sendForm);
+document.getElementById('button6').addEventListener('click', sendForm);
+document.getElementById('button7').addEventListener('click', sendForm);
+document.getElementById('button8').addEventListener('click', sendForm);
+document.getElementById('button9').addEventListener('click', sendForm);
+
+
+// function to send the result
+function sendForm() {
+    
+}

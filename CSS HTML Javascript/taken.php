eeb4b1301ec671d5209d874393786152fd39a0f5
wheelerkeith <nemimimonkey@gmail.com>
2018-10-27 17:06:34 -0500

    moved first page to new folder

---
+++ CSS HTML Javascript/taken.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        Hi, your name is <?php echo $_POST["fname"]; ?> And you live at <?php $_POST["adr"]; $_POST["city"]; $_POST["state"]; $_POST["zip"]; ?>
+    </body>
+</html>

e77fac8a91499426582ae9a38180534648c60e3e
wheelerkeith <nemimimonkey@gmail.com>
2018-11-23 09:18:15 -0600

    more css

--- css/home.js
+++ css/home.js
@@ -6,3 +6,18 @@ function changePictureSize() {
    var picWidth = document.getElementById("picWidth").value;
    document.getElementById("pic").style.width = picWidth + '%';
 }
+
+function move() {
+   var move = document.getElementById("theAnimation");
+   var pos = 0;
+   var id = setInterval(frame, 2);
+   function frame() {
+      if (pos == 350) {
+         clearInterval(id);
+      } else {
+         pos++;
+         move.style.top = pos + 'px';
+         move.style.left = pos + 'px';
+      }
+   }
+}
